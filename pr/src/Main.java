bab8b417de51e9332d88305002530b31f595c82f
marcel <marcel.feliu@gracia.lasalle.cat>
2024-11-22 13:00:39 +0100

    segon comit

--- pr/src/Main.java
+++ pr/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Aqui ha tocat el marcel!");
+        System.out.println("pau pauptauotuosiudsiofjailkds");
     }
 }
